112efd92e6bc2592d2e55e6b70ac17c37dd85c12
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-01 23:17:52 -0600

    d1p1

---
+++ days/01/day1/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+fn main() {
+    let path = String::from("./calibration_doc.txt");
+    let contents = fs::read_to_string(&path).unwrap();
+    
+    let mut products: Vec<u32> = Vec::new();
+
+    for line in contents.lines() {
+        let first_digit = get_first_digit(&line);
+        let second_digit = get_second_digit(&line);
+
+        let mut number = first_digit.to_string();
+        number.push(second_digit);
+
+        products.push(number.parse::<u32>().unwrap());
+    }
+
+    let result: u32 = products.iter().sum();
+
+    println!("Total sum {}", result);
+}
+
+fn get_first_digit(line: &str) -> char {
+    line
+        .chars()
+        .filter(|x| x.is_digit(10))
+        .next()
+        .unwrap()
+}
+
+fn get_second_digit(line: &str) -> char {
+    line
+        .chars()
+        .rev()
+        .filter(|x| x.is_digit(10))
+        .next()
+        .unwrap()
+}
