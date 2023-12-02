4e256b0ba7e63dd5f47c7ab0ce72f54dba232174
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-02 13:32:09 -0600

    d1p2 [done]

--- days/01/day1/src/main.rs
+++ days/01/day1/src/main.rs
@@ -20,12 +20,20 @@ fn main() {
         products.push(number.parse::<u32>().unwrap());
 
         let first_digit = get_first_number(&line.to_string()).unwrap();
-        
+        let second_digit = get_second_number(&line.to_string()).unwrap();
+
+        let mut number = first_digit.to_string();
+        number.push(second_digit);
+
+
+        products2.push(number.parse::<u32>().unwrap());
     }
 
     let result: u32 = products.iter().sum();
+    let result2: u32 = products2.iter().sum();
 
     println!("Total sum {}", result);
+    println!("Total sum2 {}", result2);
 }
 
 fn get_first_number(line: &String) -> Option<char> {
@@ -52,7 +60,6 @@ fn get_first_number(line: &String) -> Option<char> {
     let mut return_val = None;
 
     'outer: for (index, value) in iterator {
-        println!("line {}, index {}", line, index);
         match value {
             '0'..='9' => {
                 return_val =  Some(value);
@@ -64,7 +71,73 @@ fn get_first_number(line: &String) -> Option<char> {
         
                     for (name, name_val) in elements {
                         let index_f = index + name.len();
-                        println!("\tindex {}, name {}, line_len {}", index_f, name, line.len());
+
+                        if index_f - 1 >= line.len() {
+                            continue;
+                        }
+
+                        let peek_word =
+                            &line
+                            .chars()
+                            .map(|x| x.to_string())
+                            .collect::<Vec<String>>()
+                            [index..index_f]
+                            .join("");
+
+                        if name == peek_word {
+                            return_val = Some(*name_val);
+                            break 'outer
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return_val
+}
+
+fn get_second_number(line: &String) -> Option<char> {
+    let names = HashMap::from([
+        ('e', vec![
+            ("eno".to_string(), '1'),
+            ("eerht".to_string(), '3'),
+            ("evif".to_string(), '5'),
+            ("enin".to_string(), '9'),
+        ]),
+        ('o', vec![
+            ("owt".to_string(), '2'),
+            ("orez".to_string(), '0'),
+        ]),
+        ('r', vec![
+            ("ruof".to_string(), '4'),
+        ]),
+        ('n', vec![
+            ("neves".to_string(), '7')
+        ]),
+        ('x', vec![
+            ("xis".to_string(), '6'),
+        ]),
+        ('t', vec![
+            ("thgie".to_string(), '8')
+        ]),
+    ]);
+
+    let line = &line.chars().rev().collect::<String>();
+    let iterator = line.chars().enumerate();
+    let mut return_val = None;
+
+    'outer: for (index, value) in iterator {
+        match value {
+            '0'..='9' => {
+                return_val =  Some(value);
+                break 'outer
+            },
+            _ => {
+                if  names.contains_key(&value) {
+                    let elements = names.get(&value).unwrap();
+
+                    for (name, name_val) in elements {
+                        let index_f = index + name.len();
                         
                         if index_f - 1 >= line.len() {
                             continue;
