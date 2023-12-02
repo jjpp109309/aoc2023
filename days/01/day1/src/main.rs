dfe322b8897b5be976a026a0436f670bc645e6be
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-02 00:16:22 -0600

    create vectors for part 2

--- days/01/day1/src/main.rs
+++ days/01/day1/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let contents = fs::read_to_string(&path).unwrap();
     
     let mut products: Vec<u32> = Vec::new();
+    let mut products2: Vec<u32> = Vec::new();
 
     for line in contents.lines() {
         let first_digit = get_first_digit(&line);
@@ -14,11 +15,20 @@ fn main() {
         number.push(second_digit);
 
         products.push(number.parse::<u32>().unwrap());
+
+        let first_digit = get_first_number(&line);
+        
     }
 
     let result: u32 = products.iter().sum();
 
     println!("Total sum {}", result);
+}
+
+fn get_first_number(line: &str) -> _ {
+    for (index, value) in line.chars().enumerate() {
+        
+    }
 }
 
 fn get_first_digit(line: &str) -> char {
