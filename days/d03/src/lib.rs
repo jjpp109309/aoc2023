db322a0064623632aad0d19dde83a3cf2f5fefbe
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-03 23:17:35 -0600

    implement lexer

--- days/d03/src/lib.rs
+++ days/d03/src/lib.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn parse_input(path: &String) -> String {
+pub fn parse_input(path: &str) -> String {
 
     let input = fs::read_to_string(path)
         .unwrap()
@@ -19,6 +19,42 @@ fn parse_input(path: &String) -> String {
     let pad_string = pad_string_vec.join("");
     
     vec![pad_string.clone(), input, pad_string.clone()].join("\n")
+}
+
+struct Lexer<'a> {
+    input: std::str::Lines<'a>,
+    prv_line: String,
+    cur_line: String,
+    nxt_line: String
+}
+
+impl Lexer<'_> {
+
+    fn new(input: &String) -> Lexer {
+        let mut lexer = Lexer {
+            input: input.lines(),
+            prv_line: "".to_string(),
+            cur_line: "".to_string(),
+            nxt_line: "".to_string(),
+        };
+
+        lexer.next_line();
+        lexer.next_line();
+        lexer.next_line();
+
+        lexer
+    }
+
+    fn next_line(&mut self) {
+        self.prv_line = self.cur_line.to_owned();
+        self.cur_line = self.nxt_line.to_owned();
+        self.nxt_line = self.input.next().unwrap_or("").to_string();
+    }
+}
+
+pub fn get_part_numbers(input: &String) -> Vec<u32> {
+
+    vec![32]
 }
 
 #[cfg(test)]
@@ -41,7 +77,7 @@ mod test {
 ..664.598...
 ............".to_string();
 
-        let path = "./test_input.txt".to_string();
+        let path = "./test_input.txt";
         let input = parse_input(&path);
 
         assert_eq!(expected, input, "\nExpected:\n{} \n\n Got:\n{}", expected, input);
--- days/d03/src/main.rs
+++ days/d03/src/main.rs
@@ -1,2 +1,8 @@
+use d03::*;
+
 fn main() {
+    let path = "input.txt";
+    let input = parse_input(path);
+
+    println!("{}", input);
 }
