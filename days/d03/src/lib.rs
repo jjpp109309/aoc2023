073bb5d8773f652957388ebaa05cc8e26d0902c7
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-03 22:32:05 -0600

    parse input function

--- days/d03/src/lib.rs
+++ days/d03/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs;
+
+fn parse_input(path: &String) -> String {
+
+    let input = fs::read_to_string(path)
+        .unwrap()
+        .split_whitespace()
+        .map(|x| format!("{}{}{}", ".", x, "."))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let line_len = input.lines().next().unwrap().len();
+    println!("{}", line_len);
+
+    let mut pad_string_vec: Vec<String> = Vec::new();
+    for _ in 0..line_len {
+        pad_string_vec.push(".".to_string())
+    }
+    let pad_string = pad_string_vec.join("");
+    
+    vec![pad_string.clone(), input, pad_string.clone()].join("\n")
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn parse() {
+        let expected = "\
+............
+.467..114...
+....*.......
+...35..633..
+.......#....
+.617*.......
+......+.58..
+...592......
+.......755..
+....$.*.....
+..664.598...
+............".to_string();
+
+        let path = "./test_input.txt".to_string();
+        let input = parse_input(&path);
+
+        assert_eq!(expected, input, "\nExpected:\n{} \n\n Got:\n{}", expected, input);
     }
 }
