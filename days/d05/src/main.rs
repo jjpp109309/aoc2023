391bbbf5bbe8aed95baed067b4c58648fc62fcd3
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-06 23:52:37 -0600

    parse mappings

--- days/d05/src/main.rs
+++ days/d05/src/main.rs
@@ -3,9 +3,10 @@ use std::collections::HashMap;
 use std::ops::Range;
 use regex::Regex;
 
-struct AlmaMap<Idx> {
-    source: Range<Idx>,
-    destination: Range<Idx>,
+#[derive(Debug)]
+struct RangeMap<T> {
+    source: Range<T>,
+    destination: Range<T>,
 }
 
 fn main() {
@@ -16,7 +17,14 @@ fn main() {
 
     let seeds: Vec<u32> = parse_seeds(&input);
     println!("Test seeds: {:?}", seeds);
-    let mappings: HashMap<String, AlmaMap<u32>> = parse_mappings(&input);
+    let mappings: HashMap<String, Vec<RangeMap<u32>>> = parse_mappings(&input);
+
+    for i in mappings.get("seed-to-soil").unwrap() {
+        println!("{:?}", i);
+        for j in i.source.clone() {
+            println!("{}", j);
+        }
+    }
     
 }
 
@@ -28,14 +36,38 @@ fn parse_seeds(input: &str) -> Vec<u32> {
         .collect()
 }
 
-fn parse_mappings(input: &str) -> HashMap<String, AlmaMap<u32>> {
-    todo!()
+fn parse_mappings(input: &str) -> HashMap<String, Vec<RangeMap<u32>>> {
+    let mut range_mapping: HashMap<String, Vec<RangeMap<u32>>> = HashMap::new();
+
+    let re_maps = Regex::new(r"\w+-\w+-\w+ map:\n(\d+ \d+ \d+\n)+").unwrap();
+    let re_name = Regex::new(r"\w+-\w+-\w+").unwrap();
+    let re_ranges = Regex::new(r"\n(\d+) (\d+) (\d+)").unwrap();
+
+    let mappings_iter = re_maps.find_iter(input).map(|m| m.as_str());
+
+    for mapping in mappings_iter {
+        let name = String::from(re_name.find(mapping).unwrap().as_str());
+
+        let mut ranges: Vec<RangeMap<u32>> = Vec::new();
+
+        for range in re_ranges.captures_iter(mapping) {
+            let (_, [dst, src, len]) = range.extract();
+
+            let dst: u32 = dst.parse().unwrap();
+            let src: u32 = src.parse().unwrap();
+            let len: u32 = len.parse().unwrap();
+
+            let source = Range { start: src, end: src + len };
+            let destination = Range { start: dst, end: dst + len };
+
+            ranges.push( RangeMap { source, destination });
+        }
+
+        range_mapping.insert(name, ranges);
+    }
+
+    range_mapping
 }
-
-
-
-
-
 
 enum number {
     seed,
