a11612eec7d16651d304bbd074fc3bc3c0da7f62
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-11 23:06:03 -0600

    d06p2

--- days/d06/src/lib.rs
+++ days/d06/src/lib.rs
@@ -1,9 +1,16 @@
 use regex::Regex;
+use std::ops::{Mul, Sub};
 
 #[derive(Debug)]
 pub struct Race {
     pub time: i32,
     pub distance: i32,
+}
+
+#[derive(Debug)]
+pub struct LongRace {
+    pub time: i64,
+    pub distance: i64,
 }
 
 pub fn parse_input(string: &str) -> Vec<Race> {
@@ -25,15 +32,39 @@ pub fn parse_input(string: &str) -> Vec<Race> {
         .collect()
 }
 
+pub fn parse_single_input(string: &str) -> LongRace {
+    let re_digis = Regex::new(r"\d+").unwrap();
+    
+    let mut lines = string.lines();
+
+    let time: i64 = re_digis
+        .find_iter(lines.next().unwrap())
+        .map(|m| m.as_str().to_string())
+        .collect::<Vec<String>>()
+        .join("")
+        .parse()
+        .unwrap();
+
+    let distance: i64 = re_digis
+        .find_iter(lines.next().unwrap())
+        .map(|m| m.as_str().to_string())
+        .collect::<Vec<String>>()
+        .join("")
+        .parse()
+        .unwrap();
+
+    LongRace { time, distance }
+}
+
 pub fn total_wins(race: &Race) -> i32 {
     let mut charge_time = 0;
 
-    while total_distance(&charge_time, &race.time) <= race.distance {
+    while total_distance(charge_time, race.time) <= race.distance {
         charge_time += 1;
     }
 
     let mut wins = 0;
-    while total_distance(&charge_time, &race.time) > race.distance {
+    while total_distance(charge_time, race.time) > race.distance {
         wins += 1;
         charge_time += 1;
     }
@@ -41,6 +72,23 @@ pub fn total_wins(race: &Race) -> i32 {
     wins
 }
 
-fn total_distance(charge_time: &i32, total_time: &i32) -> i32 {
+pub fn total_long_wins(race: &LongRace) -> i64 {
+    let mut charge_time = 0;
+
+    while total_distance(charge_time, race.time) <= race.distance {
+        charge_time += 1;
+    }
+
+    let mut wins = 0;
+    while total_distance(charge_time, race.time) > race.distance {
+        wins += 1;
+        charge_time += 1;
+    }
+        
+    wins
+}
+
+fn total_distance<T> (charge_time: T, total_time: T) -> T 
+where T: Mul<Output=T> + Sub<Output=T> + Copy {
     (total_time - charge_time) * charge_time
 }
--- days/d06/src/main.rs
+++ days/d06/src/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
 use d06::{
     parse_input,
+    parse_single_input,
     total_wins,
+    total_long_wins,
 };
 
 fn main() {
@@ -28,5 +30,29 @@ fn main() {
 
     let wins: i32 = input.iter().map(|r| total_wins(r)).product();
     println!("wins: {}", wins);
+
+    // ------------------------------------------------------------------------
+    // Part 2: Test
+    // ------------------------------------------------------------------------
+    let input = match fs::read_to_string("./test.txt") {
+        Ok(string) => parse_single_input(&string),
+        Err(_) => panic!("File not found :("),
+    };
+    println!("Test input: {:?}", input);
+
+    let wins: i64 = total_long_wins(&input);
+    println!("Test wins: {}", wins);
+
+    // ------------------------------------------------------------------------
+    // Part 2
+    // ------------------------------------------------------------------------
+    let input = match fs::read_to_string("./input.txt") {
+        Ok(string) => parse_single_input(&string),
+        Err(_) => panic!("File not found :("),
+    };
+    println!("Test input: {:?}", input);
+
+    let wins: i64 = total_long_wins(&input);
+    println!("Test wins: {}", wins);
 
 }
