5684e719b77341507f3919b1e4c082abee934f43
J. Jaime Paredes <jjpp109309@gmail.com>
2023-12-23 18:23:38 -0600

    d09p2

--- days/d09/src/main.rs
+++ days/d09/src/main.rs
@@ -1,8 +1,19 @@
 use d09::*;
 
 fn main() {
-    let input = parse("./input.txt");
+    // part 1
+    let mut input = parse("./input.txt");
     let total: i64 = input.iter().map(predict).sum();
 
     println!("Part 1: {}", total);
+
+    // part 2
+    let total: i64 = input
+        .iter_mut()
+        .map(|line|{
+            line.reverse();
+            predict(line)
+        }).sum();
+
+    println!("Part 2: {}", total);
 }
