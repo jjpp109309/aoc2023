3ab9ecd79334cb06fb5424389702f57e2682805e
J. Jaime Paredes <jjpp109309@gmail.com>
2024-01-01 22:50:24 -0600

    Add function to check if point is inside polygon

--- days/d10/src/lib.rs
+++ days/d10/src/lib.rs
@@ -342,13 +342,48 @@ fn find_enclosed(
     count
 }
 
-struct Point {
-    x: i32,
-    y: i32,
+#[derive(Debug)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Point {
+    fn new(coords: &(i32, i32)) -> Self {
+        let x = coords.0;
+        let y = coords.1;
+        Point { x, y }
+    }
 }
 
 fn is_point_in_polygon(point: &Point, polygon: &Vec<(i32, i32)>) -> bool {
-    
+    let n = polygon.len();
+    let mut odd_intersections = false;
+
+    for i in 0..n {
+        let p1 = Point::new(&polygon[i]);
+        let p2 = Point::new(&polygon[(i+1) % n]);
+
+        if p1.y == p2.y {
+            let intersects_y = p1.y == point.y;
+            let intersects_x = (p1.x.min(p2.x)..=p1.x.max(p2.x)).contains(&point.x);
+
+            if intersects_y && intersects_x {
+                odd_intersections = true;
+                break;
+            }
+        } else {
+            let within_y = (p1.y.min(p2.y)..p1.y.max(p2.y)).contains(&point.y);
+            let within_x = point.x < (p2.x - p1.x) * (point.y - p1.y) / (p2.y - p1.y) + p1.x;
+
+            if within_y && within_x {
+                odd_intersections = !odd_intersections;
+            }
+        }
+
+    }
+
+    odd_intersections
 }
 
 fn loop2polygon(coords: &Vec<String>) -> Vec<(i32, i32)> {
@@ -741,36 +776,162 @@ mod test {
         panic!();
         // assert_eq!(expected, loop_path);
     }
-    
+
     #[test]
-    #[ignore]
-    fn enclosed_case2() {
-        // let graph = parse("./enclosed_2.txt");
-        // let start = "1_1".to_string();
-        // let mut loop_path = find_loop(&graph, &start, &"".to_string(), vec![]);
-        // loop_path.sort();
-        //    
-        // let (rows, cols) = format_path(&loop_path);
-        // println!("rows: {:?}", rows);
-        // println!("cols: {:?}", cols);
-        // let output = find_enclosed(&rows, &cols);
-        //       
-        // assert_eq!(output, 4);
-    }
-    
-    #[test]
-    #[ignore]
-    fn enclosed_case3() {
-        // let graph = parse("./enclosed_3.txt");
-        // let start = "4_12".to_string();
-        // let mut loop_path = find_loop(&graph, &start, &"".to_string(), vec![]);
-        // loop_path.sort();
-        //    
-        // let (rows, cols) = format_path(&loop_path);
-        // println!("rows: {:?}", rows);
-        // println!("cols: {:?}", cols);
-        // let output = find_enclosed(&rows, &cols);
-        //       
-        // assert_eq!(output, 4);
+    fn within_polygon() {
+        let polygon = vec![
+            (12, 5),
+            (13, 5),
+            (13, 4),
+            (13, 3),
+            (13, 2),
+            (13, 1),
+            (13, 0),
+            (14, 0),
+            (14, 1),
+            (14, 2),
+            (15, 2),
+            (15, 1),
+            (15, 0),
+            (16, 0),
+            (16, 1),
+            (16, 2),
+            (16, 3),
+            (15, 3),
+            (15, 4),
+            (14, 4),
+            (14, 5),
+            (15, 5),
+            (16, 5),
+            (16, 4),
+            (17, 4),
+            (17, 3),
+            (18, 3),
+            (18, 2),
+            (19, 2),
+            (19, 3),
+            (19, 4),
+            (18, 4),
+            (18, 5),
+            (17, 5),
+            (17, 6),
+            (16, 6),
+            (15, 6),
+            (15, 7),
+            (14, 7),
+            (14, 8),
+            (15, 8),
+            (15, 9),
+            (14, 9),
+            (13, 9),
+            (13, 8),
+            (13, 7),
+            (13, 6),
+            (12, 6),
+            (12, 7),
+            (12, 8),
+            (12, 9),
+            (11, 9),
+            (11, 8),
+            (11, 7),
+            (11, 6),
+            (11, 5),
+            (10, 5),
+            (10, 6),
+            (10, 7),
+            (10, 8),
+            (10, 9),
+            (9, 9),
+            (9, 8),
+            (9, 7),
+            (9, 6),
+            (8, 6),
+            (8, 7),
+            (8, 8),
+            (8, 9),
+            (7, 9),
+            (7, 8),
+            (7, 7),
+            (7, 6),
+            (6, 6),
+            (6, 7),
+            (6, 8),
+            (6, 9),
+            (5, 9),
+            (4, 9),
+            (3, 9),
+            (2, 9),
+            (1, 9),
+            (1, 8),
+            (1, 7),
+            (1, 6),
+            (0, 6),
+            (0, 5),
+            (1, 5),
+            (2, 5),
+            (3, 5),
+            (3, 6),
+            (2, 6),
+            (2, 7),
+            (2, 8),
+            (3, 8),
+            (4, 8),
+            (5, 8),
+            (5, 7),
+            (4, 7),
+            (4, 6),
+            (5, 6),
+            (5, 5),
+            (6, 5),
+            (6, 4),
+            (5, 4),
+            (4, 4),
+            (4, 3),
+            (5, 3),
+            (5, 2),
+            (5, 1),
+            (4, 1),
+            (4, 0),
+            (5, 0),
+            (6, 0),
+            (7, 0),
+            (8, 0),
+            (8, 1),
+            (7, 1),
+            (6, 1),
+            (6, 2),
+            (7, 2),
+            (7, 3),
+            (8, 3),
+            (8, 2),
+            (9, 2),
+            (9, 3),
+            (9, 4),
+            (10, 4),
+            (10, 3),
+            (10, 2),
+            (10, 1),
+            (10, 0),
+            (11, 0),
+            (11, 1),
+            (11, 2),
+            (12, 2),
+            (12, 3),
+            (11, 3),
+            (11, 4),
+            (12, 4),
+            (12, 5),
+        ];
+
+        let test_points = vec![(7, 5), (4, 5), (3, 7), (6, 3), (14, 3)];
+        let expected = vec![true, false, false, true, true];
+
+        for (test, expected) in test_points.iter().zip(expected.iter()) {
+            let point = Point::new(&test);
+            println!("point: {:?}", point);
+            let output = is_point_in_polygon(&point, &polygon);
+
+            assert_eq!(expected, &output, "cosa");
+        }
     }
 }
