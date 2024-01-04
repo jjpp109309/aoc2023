59cae6c18f612b2cf905dd0b81e388cc73230ba7
J. Jaime Paredes <jjpp109309@gmail.com>
2024-01-03 23:38:12 -0600

    final resource: lets solve it in python

--- days/d10/src/main.rs
+++ days/d10/src/main.rs
@@ -1,4 +1,5 @@
 mod display;
+use std::fs;
 use d10::*;
 
 fn main() {
@@ -7,5 +8,10 @@ fn main() {
     let start = "54_15".to_string();
     let loop_path = find_loop(&graph, &start, &"".to_string(), vec![]);
     println!("Part 1:\n{:?}", loop_path.len()/2);
+
+    let loop_string = format!("{:?}", loop_path);
+    let loop_file = "./loop.txt";
+    
+    fs::write(loop_file, loop_string).expect("path not found");
 }
 
