0e6e4cd8ca2660a9f74f8acc6452565ec810fe6e
J. Jaime Paredes <jjpp109309@gmail.com>
2024-01-06 18:26:43 -0600

    d11p1

---
+++ days/d11/src/main.rs
@@ -0,0 +1,8 @@
+use d11::*;
+
+fn main() {
+    let input = "./p1.txt";
+    let output = parse_input(input).expect("file not found");
+    let total = sum_shortest_paths(&output);
+    println!("Part 1: {}", total);
+}
