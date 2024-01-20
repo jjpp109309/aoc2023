ab31c79c3badd912803f79e0af65d70f0dcd7a49
J. Jaime Paredes <jjpp109309@gmail.com>
2024-01-20 00:36:32 -0600

    p2 works but too slow

--- days/d12/src/main.rs
+++ days/d12/src/main.rs
@@ -1,12 +1,23 @@
 use d12::*;
 
 fn main() {
-        let path = "./input.txt";
-        let onsen = parse_input(path).unwrap();
-        let sum_counts: usize = onsen.springs
-            .iter()
-            .map(|row| count_arrangements(row, 0, 0, &row.groups.clone()))
-            .sum();
+    // part 1
+    let path = "./input.txt";
+    let onsen = parse_input(path).unwrap();
+    let sum_counts: usize = onsen.springs
+        .iter()
+        .map(|row| count_arrangements(row, 0, 0, &row.groups.clone()))
+        .sum();
 
-        println!("Part 1: {}", sum_counts);
+    println!("Part 1: {}", sum_counts);
+
+    // part 2
+    let path = "./input.txt";
+    let onsen = parse_input2(path).unwrap();
+    let sum_counts: usize = onsen.springs
+        .iter()
+        .map(|row| count_arrangements(row, 0, 0, &row.groups.clone()))
+        .sum();
+
+    println!("Part 2: {}", sum_counts);
 }
